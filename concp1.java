78b38262a263c05767135575ff49a1fd13e6f2c8
Prem192002 <premsingh192002@gmail.com>
2023-05-06 23:01:54 +0530

    first commit

---
+++ concp1.java
@@ -0,0 +1,13 @@
+public class concp1 {
+    int age = 15;
+    int std=12;
+
+    public static void main(String[] args) {
+
+        concp1 rohan = new concp1();
+
+        System.out.println(rohan.age);
+        System.out.println(rohan.std);
+
+    }
+}
