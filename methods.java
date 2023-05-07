4869127ea5cd68c66fa83748c9d00f6dc7579423
Prem192002 <premsingh192002@gmail.com>
2023-05-07 19:05:34 +0530

    sum method

---
+++ methods.java
@@ -0,0 +1,23 @@
+class algebra {
+
+    int sun(int a, int b){
+        int ans = a+b;
+        return ans;
+    }
+    
+    
+}
+
+public class methods{
+    public static void main(String[] args){
+        algebra obj1 = new algebra();
+
+
+        int alg = obj1.sun(2,3);
+        System.out.println(alg);
+    }
+}
+
+
+
+
