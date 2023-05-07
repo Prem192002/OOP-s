aee25f9cc71f5d47c6e3a837d3668f74c293716e
Prem192002 <premsingh192002@gmail.com>
2023-05-07 12:03:29 +0530

    third commit

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class student{
-    int roll=12;
-    String name="prem";
+    int roll;
+    String name;
 
 }
 
@@ -12,8 +12,16 @@ public class  Main {
     public static void main(String [] args){
 
         student obj1 = new student();
+        obj1.roll=1;
+        obj1.name="Rahul";
         System.out.println(obj1.roll);
         System.out.println(obj1.name);
+
+        student obj2=new student();
+        obj2.roll=2;
+        obj2.name="rohan";
+        System.out.println(obj2.roll);
+        System.out.println(obj2.name);
     }
     
 }
