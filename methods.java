e02d80d53d2cea82858db9be6fd8213c11d4ba23
Prem192002 <premsingh192002@gmail.com>
2023-05-13 22:35:39 +0530

    input method added

--- methods.java
+++ methods.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class algebra {
 
     int sun(int a, int b){
@@ -11,9 +13,16 @@ class algebra {
 public class methods{
     public static void main(String[] args){
         algebra obj1 = new algebra();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first number");
+        int num1=sc.nextInt();
+
+        System.out.println("Enter the second number");
+        int num2=sc.nextInt();
 
 
-        int alg = obj1.sun(2,3);
+        int alg = obj1.sun(num1,num2);
         System.out.println(alg);
     }
 }
