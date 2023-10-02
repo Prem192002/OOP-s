8a23f534b30b89600a03ec17f3ce8d0a9d6ea362
Prem192002 <premsingh192002@gmail.com>
2023-10-02 19:22:03 +0530

    class and object

---
+++ studentClass.java
@@ -0,0 +1,28 @@
+
+public class studentClass {
+    static class Student{
+        String name;
+        int std;
+        int roll;
+
+    }
+    
+    public static void main(String args[]){
+        
+       
+        Student a = new Student();
+        a.name = "rahul";
+        a.std=12;
+        a.roll=1;
+        
+
+        Student b = new Student();
+        b.name = "rohan";
+        b.std = 12;
+        b.roll = 2;
+        System.out.println(a.name+" "+a.std+" "+a.roll);
+        System.out.println(b.name+" "+b.std+" "+b.roll);
+        
+    }
+    
+}
