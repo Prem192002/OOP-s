45284ad636d59d715775ab82ec5e4ff26f57f26f
Prem192002 <premsingh192002@gmail.com>
2023-10-07 23:55:53 +0530

    oops application

--- studentClass.java
+++ studentClass.java
@@ -1,27 +1,22 @@
 
 public class studentClass {
-    static class Student{
-        String name;
-        int std;
-        int roll;
-
-    }
+    
     
     public static void main(String args[]){
         
        
-        Student a = new Student();
+        student a = new student();
         a.name = "rahul";
-        a.std=12;
+        //a.std=12;
         a.roll=1;
         
 
-        Student b = new Student();
-        b.name = "rohan";
-        b.std = 12;
-        b.roll = 2;
-        System.out.println(a.name+" "+a.std+" "+a.roll);
-        System.out.println(b.name+" "+b.std+" "+b.roll);
+        //student b = new student();
+        //b.name = "rohan";
+        //b.std = 12;
+        //b.roll = 2;
+        System.out.println(a.name+" "+" "+a.roll);
+        //System.out.println(b.name+" "+" "+b.roll);
 
         
         
