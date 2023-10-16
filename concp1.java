c003c1f3cbb07902cf835fb82e9959549ca34fbc
Prem192002 <premsingh192002@gmail.com>
2023-10-16 23:05:35 +0530

    private modifier

--- concp1.java
+++ concp1.java
@@ -1,12 +1,48 @@
+import java.util.*;
+class player{
+    private String name;
+    private int age;
+    private String game;
+
+    public String getname(){
+        return name;
+    };
+    public void setname(String name){
+        this.name = name;
+    };
+
+    public int getage(){
+        return age;
+    };
+    public void setage(int age){
+        this.age = age;
+    }
+
+    public String getgame(){
+        return game;
+    }
+    public void setgame(String game){
+        this.game=game;
+    }
+}
 public class concp1 {
-    int age = 15;
-    int std=12;
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        player pl = new player();
 
-        concp1 rohan = new concp1();
+        String a = sc.nextLine();
+        pl.setname(a);
 
-        System.out.println(rohan.std);
+        int b = sc.nextInt();
+        pl.setage(b);
 
+        sc.nextLine();
+        String c=sc.nextLine();
+        pl.setgame(c);
+
+        System.out.println("name"+pl.getname());
+        System.out.println("age"+pl.getage());
+        System.out.println("game"+pl.getgame());
     }
 }
